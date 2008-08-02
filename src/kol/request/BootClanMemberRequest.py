740ed242cc76a205bae9ca7c30e952f8be8621f7
Sebastian Celis <sebastian@sebastiancelis.com>
2008-08-02 11:43:18 -0500

    Add a request for booting clan members.

---
+++ src/kol/request/BootClanMemberRequest.py
@@ -0,0 +1,11 @@
+from GenericRequest import GenericRequest
+
+class BootClanMemberRequest(GenericRequest):
+	def __init__(self, session, userId):
+		super(BootClanMemberRequest, self).__init__(session)
+		self.url = session.serverURL + "clan_members.php"
+		self.requestData['pwd'] = session.pwd
+		self.requestData['action'] = 'modify'
+		self.requestData['begin'] = '1'
+		self.requestData['pids[]'] = userId
+		self.requestData['boot%s' % userId] = 'on'
