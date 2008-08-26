70d595913345eb9ea123361d6f9889bb5d41a9bf
Sebastian Celis <sebastian@sebastiancelis.com>
2008-08-26 14:22:25 -0500

    Booting yourself from a clan is no longer useful.

--- src/kol/bot/ClanHandler.py
+++ src/kol/bot/ClanHandler.py
@@ -11,12 +11,4 @@ def botProcessChat(context, **kwargs):
 	returnCode = FilterManager.CONTINUE
 	chat = kwargs["chat"]
 	bot = kwargs["bot"]
-	
-	if chat["type"] in ["private"]:
-		if chat["text"] == "bootme":
-			r = BootClanMemberRequest(bot.session, chat["userId"])
-			r.doRequest()
-			bot.sendChatMessage("/w %s You have been booted." % chat["userId"])
-			returnCode = FilterManager.FINISHED
-		
 	return returnCode
