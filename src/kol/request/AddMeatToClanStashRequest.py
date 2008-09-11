c1beba1d25fbe41e6400490455c3162eaf8f1de3
Sebastian Celis <sebastian@sebastiancelis.com>
2008-09-11 14:41:56 -0500

    Add basic class for adding meat to the clan stash.

---
+++ src/kol/request/AddMeatToClanStashRequest.py
@@ -0,0 +1,11 @@
+from GenericRequest import GenericRequest
+
+class AddMeatToClanStashRequest(GenericRequest):
+	"Adds meat to the player's clan stash."
+	
+	def __init__(self, session, meat):
+		super(AddMeatToClanStashRequest, self).__init__(session)
+		self.url = session.serverURL + "clan_stash.php"
+		self.requestData["pwd"] = session.pwd
+		self.requestData["action"] = "contribute"
+		self.requestData["howmuch"] = meat
