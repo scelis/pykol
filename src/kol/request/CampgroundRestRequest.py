3a7c256fdc118e1d4d331e4067bf0f903a75e046
Sebastian Celis <sebastian@sebastiancelis.com>
2008-09-17 08:08:28 -0500

    Add request for resting at the user's campground.
    Thanks go to Zach Smith (ZJ) for the file.

---
+++ src/kol/request/CampgroundRestRequest.py
@@ -0,0 +1,8 @@
+from kol.request.GenericRequest import GenericRequest
+
+class CampgroundRestRequest(GenericRequest):
+	"Rests at the user's campground."
+	
+	def __init__(self, session):
+		super(CampgroundRestRequest, self).__init__(session)
+		self.url = session.serverURL + 'campground.php?action=rest'
