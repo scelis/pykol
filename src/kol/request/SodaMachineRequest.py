dbae222f2ceebd59b69c214a47805e743a6faa4c
Sebastian Celis <sebastian@sebastiancelis.com>
2008-10-03 02:12:55 +0000

    Return standard items array from SodaMachineRequest.

--- src/kol/request/SodaMachineRequest.py
+++ src/kol/request/SodaMachineRequest.py
@@ -2,12 +2,10 @@ from kol.request.GenericRequest import GenericRequest
 from kol.util import ParseResponseUtils
 
 class SodaMachineRequest(GenericRequest):
-	"Uses the soda machine in the rumpus room"
+	"Uses the soda machine in the clan rumpus room."
 	def __init__(self, session):
 		super(SodaMachineRequest, self).__init__(session)
 		self.url = session.serverURL + 'clan_rumpus.php?action=click&spot=3&furni=1'
 
 	def parseResponse(self):
-		response = ParseResponseUtils.parseItemsReceived(self.responseText, self.session)
-		
-		self.responseData = response
+		self.responseData["items"] = ParseResponseUtils.parseItemsReceived(self.responseText, self.session)
