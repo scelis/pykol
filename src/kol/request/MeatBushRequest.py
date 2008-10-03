2c38d6b0607f9acd94e91ad502c9d1aa55c3c553
Sebastian Celis <sebastian@sebastiancelis.com>
2008-10-03 02:03:50 +0000

    Cleanup MeatBushRequest.

--- src/kol/request/MeatBushRequest.py
+++ src/kol/request/MeatBushRequest.py
@@ -1,14 +1,11 @@
-from kol.request.GenericRequest import GenericRequest
+from GenericRequest import GenericRequest
 from kol.util import ParseResponseUtils
 
 class MeatBushRequest(GenericRequest):
-	"Uses the meat bush in the rumpus room"
+	"Uses the meat bush in the clan rumpus room."
 	def __init__(self, session):
 		super(MeatBushRequest, self).__init__(session)
 		self.url = session.serverURL + 'clan_rumpus.php?action=click&spot=4&furni=2'
 
 	def parseResponse(self):
-		response = {}
-		response["meat"] = ParseResponseUtils.parseMeatReceived(self.responseText)
-		
-		self.responseData = response
+		self.responseData["meat"] = ParseResponseUtils.parseMeatReceived(self.responseText)
