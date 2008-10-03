ddac2df4f1f86c10b061251743e61490b936c844
Sebastian Celis <sebastian@sebastiancelis.com>
2008-10-03 01:56:22 +0000

    Update TanULotsRequest to fully use the machine.

--- src/kol/request/TanULotsRequest.py
+++ src/kol/request/TanULotsRequest.py
@@ -1,22 +1,16 @@
-from kol.request.GenericRequest import GenericRequest
+from GenericRequest import GenericRequest
 from kol.util import ParseResponseUtils
 
-"""
-Currently this requests acts only as a click on the tanning bed.
-I do not have access to them, so if more than a simple click is required
-I will need info from someone with access to complete this request.
-"""
-
 class TanULotsRequest(GenericRequest):
-	"Visits the Tan-U-Lots Tanning Bed in the clan rumpus room"
+	"Uses the Tan-U-Lots Tanning Bed in the clan rumpus room."
 	def __init__(self, session):
 		super(TanULotsRequest, self).__init__(session)
-		self.url = session.serverURL + 'clan_rumpus.php?action=click&spot=5&furni=2'
-
+		self.url = session.serverURL + 'clan_rumpus.php'
+		self.requestData['preaction'] = 'gym'
+		self.requestData['whichgym'] = '2'
+		self.requestData['numturns'] = numTurns
+	
 	def parseResponse(self):
-		response = {}
-		respose["substats"] = ParseResponseUtils.parseSubstatsGained(self.responseText, checkMuscle=False, checkMysticality=False)
-		respose["statPoints"] = ParseResponseUtils.ParseResponseUtils.parseStatPointsGained(self.responseText, checkMuscle=False, checkMysticality=False)
-		response["level"] = ParseResponseUtils.parseLevelGained(self.responseText)
-		
-		self.responseData = response
+		self.responseData["substats"] = ParseResponseUtils.parseSubstatsGained(self.responseText, checkMuscle=False, checkMysticality=False)
+		self.responseData["stats"] = ParseResponseUtils.ParseResponseUtils.parseStatPointsGained(self.responseText, checkMuscle=False, checkMysticality=False)
+		self.responseData["level"] = ParseResponseUtils.parseLevelGained(self.responseText)
