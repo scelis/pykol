6a3889a6a7205aab8ed8e6606209c51994e3b855
Sebastian Celis <sebastian@sebastiancelis.com>
2008-10-03 01:53:54 +0000

    Update HoboFlexRequest to fully use the machine.

--- src/kol/request/HoboFlexRequest.py
+++ src/kol/request/HoboFlexRequest.py
@@ -1,22 +1,16 @@
-from kol.request.GenericRequest import GenericRequest
+from GenericRequest import GenericRequest
 from kol.util import ParseResponseUtils
 
-"""
-Currently this requests acts only as a click on the Hobo-Flex.
-I do not have access to them, so if more than a simple click is required
-I will need info from someone with access to complete this request.
-"""
-
 class HoboFlexRequest(GenericRequest):
-	"Visits the Hobo-Flex Workout Gym in the clan rumpus room"
-	def __init__(self, session):
+	"Uses the Hobo-Flex Workout Gym to increase the user's muscle."
+	def __init__(self, session, numTurns):
 		super(HoboFlexRequest, self).__init__(session)
-		self.url = session.serverURL + 'clan_rumpus.php?action=click&spot=9&furni=1'
+		self.url = session.serverURL + 'clan_rumpus.php'
+		self.requestData['preaction'] = 'gym'
+		self.requestData['whichgym'] = '3'
+		self.requestData['numturns'] = numTurns
 
 	def parseResponse(self):
-		response = {}
-		respose["substats"] = ParseResponseUtils.parseSubstatsGained(self.responseText, checkMysticality=False, checkMoxie=False)
-		respose["statPoints"] = ParseResponseUtils.ParseResponseUtils.parseStatPointsGained(self.responseText, checkMysticality=False, checkMoxie=False)
-		response["level"] = ParseResponseUtils.parseLevelGained(self.responseText)
-		
-		self.responseData = response
+		self.responseData["substats"] = ParseResponseUtils.parseSubstatsGained(self.responseText, checkMysticality=False, checkMoxie=False)
+		self.responseData["stats"] = ParseResponseUtils.ParseResponseUtils.parseStatPointsGained(self.responseText, checkMysticality=False, checkMoxie=False)
+		self.responseData["level"] = ParseResponseUtils.parseLevelGained(self.responseText)
