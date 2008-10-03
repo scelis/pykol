f95f2dcc96e30e6a9e4d22cff22fa0548f2bdf83
Sebastian Celis <sebastian@sebastiancelis.com>
2008-10-03 01:47:53 +0000

    Update ArcaneTomesRequest.

--- src/kol/request/ArcaneTomesRequest.py
+++ src/kol/request/ArcaneTomesRequest.py
@@ -1,22 +1,16 @@
-from kol.request.GenericRequest import GenericRequest
+from GenericRequest import GenericRequest
 from kol.util import ParseResponseUtils
-
-"""
-Currently this requests acts only as a click on the tomes.
-I do not have access to them, so if more than a simple click is required
-I will need info from someone with access to complete this request.
-"""
 
 class ArcaneTomesRequest(GenericRequest):
 	"Visits the shelf of arcane tomes in the clan rumpus room"
-	def __init__(self, session):
+	def __init__(self, session, numTurns):
 		super(ArcaneTomesRequest, self).__init__(session)
-		self.url = session.serverURL + 'clan_rumpus.php?action=click&spot=2&furni=1'
+		self.url = session.serverURL + 'clan_rumpus.php'
+		self.requestData['preaction'] = 'gym'
+		self.requestData['whichgym'] = '1'
+		self.requestData['numturns'] = numTurns
 
 	def parseResponse(self):
-		response = {}
-		respose["substats"] = ParseResponseUtils.parseSubstatsGained(self.responseText, checkMuscle=False, checkMoxie=False)
-		respose["statPoints"] = ParseResponseUtils.ParseResponseUtils.parseStatPointsGained(self.responseText, checkMuscle=False, checkMoxie=False)
-		response["level"] = ParseResponseUtils.parseLevelGained(self.responseText)
-		
-		self.responseData = response
+		self.responseData["substats"] = ParseResponseUtils.parseSubstatsGained(self.responseText, checkMuscle=False, checkMoxie=False)
+		self.responseData["stats"] = ParseResponseUtils.ParseResponseUtils.parseStatPointsGained(self.responseText, checkMuscle=False, checkMoxie=False)
+		self.responseData["level"] = ParseResponseUtils.parseLevelGained(self.responseText)
