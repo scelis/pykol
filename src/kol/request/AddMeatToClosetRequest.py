fce9cc3f7573ac07402e428d1437009a3b52303c
Sebastian Celis <sebastian@sebastiancelis.com>
2009-02-27 14:17:19 +0000

    Add closet functionality.
    These new requests allow the user to deposit and withdraw meat from
    the user's closet.

---
+++ src/kol/request/AddMeatToClosetRequest.py
@@ -0,0 +1,11 @@
+from GenericRequest import GenericRequest
+
+class AddMeatToClosetRequest(GenericRequest):
+	"Adds meat to the player's closet."
+	
+	def __init__(self, session, meat=""):
+		super(AddMeatToClanStashRequest, self).__init__(session)
+		self.url = session.serverURL + "closet.php"
+		self.requestData["pwd"] = session.pwd
+		self.requestData["action"] = "addmeat"
+		self.requestData["amt"] = meat
---
+++ src/kol/request/TakeMeatFromClosetRequest.py
@@ -0,0 +1,11 @@
+from GenericRequest import GenericRequest
+
+class TakeMeatFromClosetRequest(GenericRequest):
+	"Adds meat to the player's closet."
+	
+	def __init__(self, session, meat=""):
+		super(AddMeatToClanStashRequest, self).__init__(session)
+		self.url = session.serverURL + "closet.php"
+		self.requestData["pwd"] = session.pwd
+		self.requestData["action"] = "takemeat"
+		self.requestData["amt"] = meat
