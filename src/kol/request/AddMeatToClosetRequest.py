32e844266a8b15b6d7506b62393c074e567b505c
Sebastian Celis <sebastian@sebastiancelis.com>
2009-02-27 14:23:01 +0000

    Fix some errors with closet requests.

--- src/kol/request/AddMeatToClosetRequest.py
+++ src/kol/request/AddMeatToClosetRequest.py
@@ -4,7 +4,7 @@ class AddMeatToClosetRequest(GenericRequest):
 	"Adds meat to the player's closet."
 	
 	def __init__(self, session, meat=""):
-		super(AddMeatToClanStashRequest, self).__init__(session)
+		super(AddMeatToClosetRequest, self).__init__(session)
 		self.url = session.serverURL + "closet.php"
 		self.requestData["pwd"] = session.pwd
 		self.requestData["action"] = "addmeat"
--- src/kol/request/TakeMeatFromClosetRequest.py
+++ src/kol/request/TakeMeatFromClosetRequest.py
@@ -4,7 +4,7 @@ class TakeMeatFromClosetRequest(GenericRequest):
 	"Adds meat to the player's closet."
 	
 	def __init__(self, session, meat=""):
-		super(AddMeatToClanStashRequest, self).__init__(session)
+		super(TakeMeatFromClosetRequest, self).__init__(session)
 		self.url = session.serverURL + "closet.php"
 		self.requestData["pwd"] = session.pwd
 		self.requestData["action"] = "takemeat"
