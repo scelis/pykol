5115097cfe9f89f39730076888330a63087a3a92
Dave Lee <camperdave614@gmail.com>
2011-04-14 10:30:09 -0500

    Added a new request for the VIP Klaw Machine.

---
+++ src/kol/request/DeluxeMrKlawRequest.py
@@ -0,0 +1,11 @@
+from GenericRequest import GenericRequest
+from kol.util import ParseResponseUtils
+
+class DeluxeMrKlawRequest(GenericRequest):
+    "Uses the Deluxe Mr. Klaw in the clan VIP room."
+    def __init__(self, session):
+        super(DeluxeMrKlawRequest, self).__init__(session)
+        self.url = session.serverURL + 'clan_viplounge.php?action=klaw'
+
+    def parseResponse(self):
+        self.responseData["items"] = ParseResponseUtils.parseItemsReceived(self.responseText, self.session)
