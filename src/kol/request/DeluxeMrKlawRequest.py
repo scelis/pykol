40637652f9d9f485e2ff49a2c9531298bf4f14d6
Sebastian Celis <sebastian@sebastiancelis.com>
2011-05-02 18:59:42 -0500

    Use requestData dictionary for URL parameters.

--- src/kol/request/DeluxeMrKlawRequest.py
+++ src/kol/request/DeluxeMrKlawRequest.py
@@ -5,7 +5,8 @@ class DeluxeMrKlawRequest(GenericRequest):
     "Uses the Deluxe Mr. Klaw in the clan VIP room."
     def __init__(self, session):
         super(DeluxeMrKlawRequest, self).__init__(session)
-        self.url = session.serverURL + 'clan_viplounge.php?action=klaw'
+        self.url = session.serverURL + 'clan_viplounge.php'
+        self.requestData["action"] = "klaw"
 
     def parseResponse(self):
         self.responseData["items"] = ParseResponseUtils.parseItemsReceived(self.responseText, self.session)
