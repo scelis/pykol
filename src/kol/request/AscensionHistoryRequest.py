0ef37999549a922372c073edbb29c5f64320b58a
natnit <natnit@gmail.com>
2011-05-22 10:22:02 -0700

    Updates to AscensionHistory. Tested and working for Bee runs.
    Added request to allow lookup of player name from kolID.

--- src/kol/request/AscensionHistoryRequest.py
+++ src/kol/request/AscensionHistoryRequest.py
@@ -16,45 +16,57 @@ class AscensionHistoryRequest(GenericRequest):
         keys:
 
               id -- The ascension #.
-           start -- The start date of the ascension (as datetime).
-             end -- The end date of the ascension (as datetime).
+           start -- The start date of the ascension (as date).
+             end -- The end date of the ascension (as date).
            level -- The level at which the user ascended.
-           class -- The class of the user during the ascension
+       charClass -- The class of the user during the ascension
             sign -- The sign of the user during the ascension
            turns -- The number of turns the ascension lasted
             days -- The number of days the ascension lasted
         familiar -- The most used familiar this ascension
         famUsage -- The use percentage of the most used familiar
-            type -- The type of ascension (normal, hardcore, casual, BM)
+            mode -- The mode of ascension (normal, hardcore, casual, BM)
             path -- The path of the ascension (teet, booze, oxy)
         """
-
+        
         fullAscensionPattern = PatternManager.getOrCompilePattern('fullAscension')
         famPattern = PatternManager.getOrCompilePattern('familiarAscension')
-
-        ascensions = []
-
+        namePattern = PatternManager.getOrCompilePattern('playerName')
+        
+        
         stripText = self.responseText.replace("&nbsp;", "")
+        
+        for names in namePattern.finditer(stripText):
+            self.responseData["name"] = names.group(1).strip().lower()
+        
+        ascensions = []
+        
         for ascension in fullAscensionPattern.finditer(stripText):
             ascNumber = ascension.group(1)
             ascDate = ascension.group(2)
             ascLevel = int(ascension.group(3))
             ascClass = ascension.group(4).strip()
             ascSign = ascension.group(5).strip()
-            ascTurns = int(ascension.group(6).replace(',',''))
-            ascDays = int(ascension.group(7).replace(',',''))
-            ascFamiliarData = ascension.group(8)
-            ascType = ascension.group(10)
-            ascPath = ascension.group(12)
-
+            ascTurns = int(ascension.group(7).replace(',',''))
+            ascDays = int(ascension.group(10).replace(',',''))
+            ascFamiliarData = ascension.group(12)
+            ascMode = ascension.group(14)
+            ascPath = ascension.group(16)
+            
             try:
                 ascEnd = datetime.strptime(ascDate, "%m/%d/%y")
             except ValueError:
                 ascEnd = dateStr
-
+        
             runlength = timedelta(ascDays - 1)
             ascStart = ascEnd - runlength
-
+            
+            ascStart = ascStart.date()
+            ascEnd = ascEnd.date()
+            
+            ascFamiliar = ''
+            ascFamUsage = ''
+    
             if ascFamiliarData == "No Data":
                 ascFamiliar = "None"
                 ascFamUsage = 0
@@ -62,13 +74,17 @@ class AscensionHistoryRequest(GenericRequest):
                 for match in famPattern.finditer(ascFamiliarData):
                     ascFamiliar = match.group(1).strip()
                     ascFamUsage = match.group(2)
-
-            if ascType == "Hardcore" and ascPath == "Bad Moon":
-                ascType = "Bad Moon"
+    
+            if ascMode == "Hardcore" and ascPath == "Bad Moon":
+                ascMode = "Bad Moon"
                 ascPath = "None"
-
-            asc = {"id":ascNumber, "start":ascStart, "end":ascEnd, "level":ascLevel, "class":ascClass, "sign":ascSign, "turns":ascTurns, "days":ascDays, "familiar":ascFamiliar, "famUsage":ascFamUsage, "type":ascType, "path":ascPath}
-
+            if (not ascMode):
+                ascMode = "Softcore"
+            if (not ascPath):
+                ascPath = "None"
+        
+            asc = {"id":ascNumber, "start":ascStart, "end":ascEnd, "level":ascLevel, "charClass":ascClass, "sign":ascSign, "turns":ascTurns, "days":ascDays, "familiar":ascFamiliar, "famUsage":ascFamUsage, "mode":ascMode, "path":ascPath}
+            
             ascensions.append(asc)
-
+        
         self.responseData["ascensions"] = ascensions
