add8018734d8941302a93b0ef5ce813fc5704fad
Gustav Bertram <gustav.bertram@gmail.com>
2011-07-18 13:09:13 +0200

    Added two more Clan VIP requests - GWB

---
+++ src/kol/request/CrimboTreeRequest.py
@@ -0,0 +1,12 @@
+from GenericRequest import GenericRequest
+from kol.util import ParseResponseUtils
+
+class CrimboTreeRequest(GenericRequest):
+    "Uses the Crimbo Tree in the clan VIP room."
+    def __init__(self, session):
+        super(DeluxeMrKlawRequest, self).__init__(session)
+        self.url = session.serverURL + 'clan_viplounge.php'
+        self.requestData["action"] = "crimbotree"
+
+    def parseResponse(self):
+        self.responseData["items"] = ParseResponseUtils.parseItemsReceived(self.responseText, self.session)
---
+++ src/kol/request/LookingGlassRequest.py
@@ -0,0 +1,12 @@
+from GenericRequest import GenericRequest
+from kol.util import ParseResponseUtils
+
+class LookingGlassRequest(GenericRequest):
+    "Uses the Looking Glass in the clan VIP room."
+    def __init__(self, session):
+        super(DeluxeMrKlawRequest, self).__init__(session)
+        self.url = session.serverURL + 'clan_viplounge.php'
+        self.requestData["action"] = "lookingglass"
+
+    def parseResponse(self):
+        self.responseData["items"] = ParseResponseUtils.parseItemsReceived(self.responseText, self.session)
